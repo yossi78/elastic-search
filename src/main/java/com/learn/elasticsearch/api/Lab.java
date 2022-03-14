6fbcb544ad06d0330ac013984f55a7d2577b8b4e
ytal <yossi.tal@harman.com>
2022-03-14 12:02:25 +0200

    doCommit

--- src/main/java/com/learn/elasticsearch/api/Lab.java
+++ src/main/java/com/learn/elasticsearch/api/Lab.java
@@ -4,6 +4,13 @@ public class Lab {
 
 
 
+
+
+    public void UndoCommit(){
+        System.out.println("UndoCommit");
+    }
+
+
     public static void main(String[] args) {
         System.out.println("TEST-1");
     }
